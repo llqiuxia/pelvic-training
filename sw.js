eac33b18d7c151a3154e0278f3ba85e38e5105e3
llqiuxia <llqiuxia@users.noreply.github.com>
2026-06-15 17:17:28 +0800

    Initial commit: 盆底肌分级训练助手

---
+++ sw.js
@@ -0,0 +1,28 @@
+var CACHE_NAME = "pelvic-trainer-v1";
+var URLS_TO_CACHE = [
+  "index.html",
+  "manifest.json"
+];
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(n) { return n !== CACHE_NAME; }).map(function(n) { return caches.delete(n); })
+      );
+    })
+  );
+});
